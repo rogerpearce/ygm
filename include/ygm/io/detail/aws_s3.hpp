59af27b2877097849c925c5dd8459820fafe842c
Roger Pearce <rpearce@llnl.gov>
2023-04-20 22:39:16 -0500

    Update aws_s3.hpp

--- include/ygm/io/detail/aws_s3.hpp
+++ include/ygm/io/detail/aws_s3.hpp
@@ -42,7 +42,7 @@ class aws_line_reader {
          break;
        }
        using namespace std::chrono_literals;
-       std::this_thread::sleep_for(5ms)
+       std::this_thread::sleep_for(5ms);
      }
     } while(!outcome.IsSuccess());
 
